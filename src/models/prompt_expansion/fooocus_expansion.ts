9feb780451115b97f81e1d0d659965471c6e8b5d
dong.huo <huodon@gmail.com>
2023-09-30 14:35:24 +0800

    WIP

--- src/models/prompt_expansion/fooocus_expansion.ts
+++ src/models/prompt_expansion/fooocus_expansion.ts
@@ -1,4 +1,25 @@
+import { basename } from "path";
 import { Model } from "~/types";
+
+async function getCodeList() {
+
+  const branch = "moonride-main";
+
+  const data = await fetch(
+    `https://api.github.com/repos/MoonRide303/Fooocus-MRE/git/trees/${branch}?recursive=1`
+  ).then((it) => it.json<{ tree: { path: string }[] }>());
+
+  return data.tree
+    .filter((it) =>
+      it.path.startsWith("models/prompt_expansion/fooocus_expansion/")
+    )
+    .map((it) => ({
+      url: `https://raw.githubusercontent.com/MoonRide303/Fooocus-MRE/${branch}/${it.path}`,
+      filename: basename(it.path),
+    }));
+}
+
+const sources = await getCodeList();
 
 export default {
   huggingface: [
@@ -8,4 +29,5 @@ export default {
       alias: "pytorch_model",
     },
   ],
+  url: [...sources],
 } as Model;
