f2c3f7a10a8b0698ab87be8d2694c071f524f173
huodon <huodon@gmail.com>
2023-11-08 15:11:40 +0800

    WIP

--- src/manifest/custom_nodes/ComfyUI_IPAdapter_plus/models.ts
+++ src/manifest/custom_nodes/ComfyUI_IPAdapter_plus/models.ts
@@ -6,19 +6,21 @@ export default {
     // IP-Adapter
     // https://github.com/cubiq/ComfyUI_IPAdapter_plus
     ...[
-      'models/ip-adapter_sd15.bin',
-      'models/ip-adapter_sd15_light.bin',
-      'models/ip-adapter-plus_sd15.bin',
-      'models/ip-adapter-plus-face_sd15.bin',
-      'sdxl_models/ip-adapter_sdxl.bin',
-
+      'models/ip-adapter_sd15',
+      'models/ip-adapter_sd15_light',
+      'models/ip-adapter-plus_sd15',
+      'models/ip-adapter-plus-face_sd15',
+      // ------------------------------------------
+      // sdxl
+      'sdxl_models/ip-adapter_sdxl',
       // This model requires the use of the SD1.5 encoder despite being for SDXL checkpoints
-      'sdxl_models/ip-adapter_sdxl_vit-h.bin',
+      'sdxl_models/ip-adapter_sdxl_vit-h',
       // Same as obove
-      'sdxl_models/ip-adapter-plus_sdxl_vit-h.bin',
+      'sdxl_models/ip-adapter-plus_sdxl_vit-h',
+      'sdxl_models/ip-adapter-plus-face_sdxl_vit-h'
     ].map((it) => ({
       repo_id: 'h94/IP-Adapter',
-      filename: it,
+      filename: `${it}.safetensors`,
     })),
   ],
 } satisfies Model
